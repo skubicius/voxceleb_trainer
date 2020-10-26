c62e7d3094ae106b573a35942c642168e4860362
Szymon <skubicius@gmail.com>
2020-10-26 22:47:10 +0100

    adjust name

--- SpeakerNet.py
+++ SpeakerNet.py
@@ -140,7 +140,9 @@ class SpeakerNet(nn.Module):
 
             # import pickle
             # pickle.dump(res,  open( f"{file}.p", "wb" ) )
-            with open(f'{file}.npy', 'wb') as f:
+
+            fn = ''.join(file.split('.')[:-1])
+            with open(f'{fn}.npy', 'wb') as f:
                 numpy.save(f, res)
 
             filename = '%06d.wav'%idx
